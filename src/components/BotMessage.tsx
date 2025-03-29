c9a71932a6fd8866641f997898f8e41eeddc282b
isurueranga-98 <isurueranga.0@gmail.com>
2025-03-30 03:37:16 +0530

    refactor: update BotMessage to use ReactMarkdown for rendering message text

--- src/components/BotMessage.tsx
+++ src/components/BotMessage.tsx
@@ -1,5 +1,23 @@
+// import React from 'react';
+// //import { MemoizedMarkdown } from './Markdown';
+// import { TMessage } from '../types';
+// import { UserTypes } from '../enums';
+
+// const BotMessage: React.FC<{ message: TMessage }> = ({ message }) => {
+//     if (message.sender !== UserTypes.BOT) {
+//         return null;
+//     }
+
+//     return (
+//         <div className="w-auto max-w-full self-start inline-block text-left mx-2 my-2 bg-gray-100 rounded-lg p-4 shadow">
+//             {message.text} 
+//         </div>
+//     );
+// };
+
+// export default BotMessage;
 import React from 'react';
-//import { MemoizedMarkdown } from './Markdown';
+import ReactMarkdown from 'react-markdown'; // Add this import
 import { TMessage } from '../types';
 import { UserTypes } from '../enums';
 
@@ -10,7 +28,10 @@ const BotMessage: React.FC<{ message: TMessage }> = ({ message }) => {
 
     return (
         <div className="w-auto max-w-full self-start inline-block text-left mx-2 my-2 bg-gray-100 rounded-lg p-4 shadow">
-            {message.text} 
+            {/* Replace plain text rendering with ReactMarkdown */}
+            <ReactMarkdown>
+                {message.text}
+            </ReactMarkdown>
         </div>
     );
 };
